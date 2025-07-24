8413c93d54e806d60d4d7e4d3194bce76e584e42
kanaimandal2002 <148377285+kanaimandal2002@users.noreply.github.com>
2025-07-24 17:32:53 +0530

    Update Poll.js

--- server/models/Poll.js
+++ server/models/Poll.js
@@ -1,1 +1,30 @@
+const mongoose = require('mongoose');
 
+const optionSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true
+  },
+  votes: {
+    type: Number,
+    default: 0
+  }
+});
+
+const pollSchema = new mongoose.Schema({
+  question: {
+    type: String,
+    required: true
+  },
+  options: [optionSchema],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  totalVotes: {
+    type: Number,
+    default: 0
+  }
+});
+
+module.exports = mongoose.model('Poll', pollSchema);
