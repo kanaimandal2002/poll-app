1079029e4b9facd7823ea940501af4deddcf88a3
kanaimandal2002 <148377285+kanaimandal2002@users.noreply.github.com>
2025-07-27 20:51:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CreatePoll from './pages/CreatePoll';
+import Poll from './pages/Poll';
+import Home from './pages/Home';
 
+function App() {
+  return (
+    <Router>
+      <div className="container mx-auto px-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<CreatePoll />} />
+          <Route path="/poll/:id" element={<Poll />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
